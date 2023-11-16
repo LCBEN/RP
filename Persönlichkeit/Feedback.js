35fd56e9f2f17d9100da6706e40f5f68f7507d98
LCBEN <131679024+LCBEN@users.noreply.github.com>
2023-11-16 08:40:48 +0100

    Create Feedback.js

---
+++ Persönlichkeit/Feedback.js
@@ -0,0 +1,34 @@
+function calculateTotal() {
+  let total = 0;
+  const keys = [1, 4, 9, 11, 16, 21, 22, 28]; // Array mit den spezifischen Schlüsselnummern
+
+  keys.forEach(function(key) {
+    total += parseFloat(sessionStorage.getItem('selectedAnswer' + key)) || 0;
+  });
+
+  return total;
+}
+
+function getFeedback(total) {
+  if (total > 0) {
+    return "Was hier los";
+  } else {
+    return "Test";
+  }
+}
+
+
+function displayFeedback() {
+  // Berechnen Gesamtwert
+  const total = calculateTotal();
+
+  // Feedback-Text basierend auf dem Gesamtwert
+  const feedbackText = getFeedback(total);
+
+  // Den Feedback-Text auf der Seite anzeigen
+  const feedbackElement = document.querySelector('#feedback');
+  feedbackElement.innerHTML = '<b>' + feedbackText + '</b>';
+}
+  document.addEventListener('DOMContentLoaded', function() {
+  displayFeedback();
+});
